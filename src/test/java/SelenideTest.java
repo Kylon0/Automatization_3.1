f943ab4673d3eb917035a6174926b48cc168e284
Kylon0 <wladikylon@gmail.com>
2022-02-27 23:14:19 +0300

    Initial commit 2

---
+++ src/test/java/SelenideTest.java
@@ -0,0 +1,22 @@
+import org.junit.jupiter.api.Test;
+
+import com.codeborne.selenide.*;
+
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Selenide.*;
+
+
+public class SelenideTest {
+
+    @Test
+    void shouldSubmitRequest(){
+        open("http://localhost:9999");
+        SelenideElement form = $("[id=root]");
+        form.$("[data-test-id=name] input").setValue("Иванов Иван");
+        form.$("[data-test-id=phone] input").setValue("+79533556677");
+        form.$("[data-test-id=agreement]").click();
+        form.$("[type=button]").click();
+        $("[data-test-id=order-success]").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+    }
+
+}
